33898892b7a8d98ffcec48bad2d74b09dbcb7394
pviscone <pieroviscone@gmail.com>
2022-06-10 19:25:22 +0200

    docs: Added mainpage to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*! \mainpage TrackG4PS
+ *
+ * \section Geant4_simulation
+ * In this project we simulate the commissioning of the PS module for the CMS detector.
+ *
+ * A PS module is composed by a layer of strips and a layer of pixels.
+ * In this project we simulate a telescope for cosmic muons build with the PS modules.
+ * \section Tracking
+ * After the simulation the track are reconstructed using the Hough transform
+ */
+
+
+
 int main(){
     return 0;
 
