9e8c96c85f59ac6dceddd84ae99b98e96b39d9c7
pviscone <pieroviscone@gmail.com>
2022-06-10 15:42:49 +0200

    updated the local documentation

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['mymaterials_0',['MyMaterials',['../struct_my_materials.html',1,'']]],
+  ['myprimarygenerator_1',['MyPrimaryGenerator',['../class_my_primary_generator.html',1,'']]]
+];
