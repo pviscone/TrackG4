d9b986f1fe54fe1cac7ea0637289d06c35b28fff
pviscone <pieroviscone@gmail.com>
2022-06-10 15:41:45 +0200

    feat: create Physics.hh

---
+++ Geant4/include/Physics.hh
@@ -0,0 +1,15 @@
+#pragma once
+#include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics.hh"
+#include "G4DecayPhysics.hh"
+
+/** @class MyPhysicsList
+ * @brief The PhysicsList class
+ *
+ * This class contains the physics processes used in the simulation.
+ */
+class MyPhysicsList : public G4VModularPhysicsList{
+    public:
+        MyPhysicsList();
+        ~MyPhysicsList();
+};
