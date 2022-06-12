0fb0ffd3f2b929fa0917009e8b6f5eee7c0e39ae
pviscone <pieroviscone@gmail.com>
2022-06-12 19:53:54 +0200

    docs: Added documentation to action.hh

--- Geant4/include/Action.hh
+++ Geant4/include/Action.hh
@@ -1,14 +1,41 @@
 #pragma once
 #include "G4VUserActionInitialization.hh"
 #include "Generator.hh"
-//#include "run.hh"
+#include "RunAction.hh"
 
+/**
+ * @brief The class that manages the action initialization.
+ *
+ * The action initialization is the class that manages the action initialization.
+ *
+ *
+ */
 class MyActionInitialization : public G4VUserActionInitialization{
     public:
+        /**
+         * @brief Constructor of the action initialization.
+         *
+         * @param generator The generator of the particles.
+         */
         MyActionInitialization();
+        /**
+         * @brief Destructor of the action initialization.
+         */
         ~MyActionInitialization();
 
-        //Runna la particle gun e si occupa dello stepping (build per ogni thread, build for master solo per il master thread)
+        /**
+         * @brief Method that is called when the action initialization is built.
+         *
+         * This method runs at the beginning of the program and it is used to build the action initialization.
+         * In multithreading, the method is called by each thread.
+         */
         virtual void Build() const;
-        //virtual void BuildForMaster() const;
+
+        /**
+         * @brief Method that is called when the action initialization is built for the master thread.
+         *
+         * This method runs at the beginning of the program and it is used to build the action initialization for the master thread.
+         * In multithreading, the method is called by the master thread.
+         */
+        virtual void BuildForMaster() const;
 };
