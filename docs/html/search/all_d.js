c24054f9358cb94f1928dcfcfc518b1a3eca3745
pviscone <pieroviscone@gmail.com>
2022-06-16 07:41:18 +0200

    Updated local documentation

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['test_5fcase_0',['TEST_CASE',['../_data_manager_test_8cpp.html#a5d2e85582a54c535325ca7cb060e4356',1,'TEST_CASE(&quot;Verify that DataManager is a singleton&quot;):&#160;DataManagerTest.cpp'],['../_data_manager_m_t_test_8cpp.html#aa13251e05e7143e9927082b47bd166b6',1,'TEST_CASE(&quot;Verify that DataManagerMT is a singleton&quot;):&#160;DataManagerMTTest.cpp']]],
+  ['test_5fcase_0',['TEST_CASE',['../_data_manager_test_8cpp.html#a5d2e85582a54c535325ca7cb060e4356',1,'TEST_CASE(&quot;Verify that DataManager is a singleton&quot;):&#160;DataManagerTest.cpp'],['../_data_manager_m_t_test_8cpp.html#aa13251e05e7143e9927082b47bd166b6',1,'TEST_CASE(&quot;Verify that DataManagerMT is a singleton&quot;):&#160;DataManagerMTTest.cpp'],['../_data_manager_m_t_test_8cpp.html#a4690fb3465c8781e22d98f95903e911e',1,'TEST_CASE(&quot;Verify that DataManagerMt has a vector of different instances of DataManager&quot;):&#160;DataManagerMTTest.cpp']]],
   ['theta_1',['theta',['../class_truth_beam_data.html#a42cae571a42f6337db40e7a65f9e4cab',1,'TruthBeamData']]],
   ['trackg4ps_2',['TrackG4PS',['../index.html',1,'']]],
   ['trackid_3',['TrackID',['../class_detector_data.html#a6edbd0d091bd36d579c556dd0fc7d980',1,'DetectorData']]],
--- docs/html/search/functions_8.js
+++ docs/html/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['test_5fcase_0',['TEST_CASE',['../_data_manager_test_8cpp.html#a5d2e85582a54c535325ca7cb060e4356',1,'TEST_CASE(&quot;Verify that DataManager is a singleton&quot;):&#160;DataManagerTest.cpp'],['../_data_manager_m_t_test_8cpp.html#aa13251e05e7143e9927082b47bd166b6',1,'TEST_CASE(&quot;Verify that DataManagerMT is a singleton&quot;):&#160;DataManagerMTTest.cpp']]],
+  ['test_5fcase_0',['TEST_CASE',['../_data_manager_test_8cpp.html#a5d2e85582a54c535325ca7cb060e4356',1,'TEST_CASE(&quot;Verify that DataManager is a singleton&quot;):&#160;DataManagerTest.cpp'],['../_data_manager_m_t_test_8cpp.html#aa13251e05e7143e9927082b47bd166b6',1,'TEST_CASE(&quot;Verify that DataManagerMT is a singleton&quot;):&#160;DataManagerMTTest.cpp'],['../_data_manager_m_t_test_8cpp.html#a4690fb3465c8781e22d98f95903e911e',1,'TEST_CASE(&quot;Verify that DataManagerMt has a vector of different instances of DataManager&quot;):&#160;DataManagerMTTest.cpp']]],
   ['truthbeamdata_1',['TruthBeamData',['../class_truth_beam_data.html#a8246e42598cab028ef174580b9bdba86',1,'TruthBeamData']]]
 ];
