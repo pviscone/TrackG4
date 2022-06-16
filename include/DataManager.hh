0b04e665a8c1442b4d0aafbd0e8da7ca3f43ff65
pviscone <pieroviscone@gmail.com>
2022-06-16 06:47:59 +0200

    Created DataManager singletor to manage the user Event data

---
+++ include/DataManager.hh
@@ -0,0 +1,22 @@
+#pragma once
+#include "TFile.h"
+#include "TTree.h"
+#include "EventData.hh"
+
+class DataManager{
+    private:
+        DataManager();
+        static DataManager *instance;
+        TFile *file;
+        TTree *tree;
+        Event *event;
+        friend class DataManagerMT;
+    public:
+        static DataManager *GetInstance();
+        ~DataManager();
+        TFile *OpenFile(std::string filename, std::string option="");
+        TTree *CreateTree(std::string name);
+        TFile *GetFile();
+        TTree *GetTree();
+        Event *GetEvent();
+};
