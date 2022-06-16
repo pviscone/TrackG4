635c8f0af6ee9fccdc613853ab751b8384195383
pviscone <pieroviscone@gmail.com>
2022-06-16 06:39:00 +0200

    build script run geant  in batch mode. Then concatenate the files

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 THREADS=`grep -c ^processor /proc/cpuinfo`
 mkdir -p ./build
 cd ./build
+rm ./output*.root
 cmake ..
 make -j$THREADS
 ./tests_executable -s
+./CosmicMuSimulation run.mac
+hadd ./output.root ./output*.root
