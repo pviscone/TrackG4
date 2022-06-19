ded02517aa6e367c55508432b8175984fc51fbe1
pviscone <pieroviscone@gmail.com>
2022-06-19 22:18:31 +0200

    Added a coincidence trigger on EndOfEventAction

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -11,6 +11,7 @@ void MyEventAction::BeginOfEventAction(const G4Event *)
 /**
  * At the end of each action this function fill the tree and clear the event object
  *
+ * If there aren't at least two hit in two different layers, the event is not saved
  */
 void MyEventAction::EndOfEventAction(const G4Event *)
 {
@@ -18,6 +19,15 @@ void MyEventAction::EndOfEventAction(const G4Event *)
     DataManagerMT *dataManagerMT = DataManagerMT::GetInstance();
     int threadId = G4Threading::G4GetThreadId();
     DataManager *dataManager = dataManagerMT->GetSTDataManager(threadId);
+
+    //Trigger: if the event has not at least two hit in two different layers, it is not saved
+    Event *ev = dataManager->GetEvent();
+    std::vector<int> layerVec=ev->detectorData.Layer;
+    if(! layerVec.empty()){
+        if (std::max_element(layerVec.begin(), layerVec.end()) == std::min_element(layerVec.begin(),layerVec.end() ) ) {
+            (ev->detectorData).Clear();
+        }
+    }
 
     // Fill the tree and clear the event
     TTree *tree = dataManager->GetTree();
